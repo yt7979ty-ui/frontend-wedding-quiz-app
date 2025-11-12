544c238ee4179c6420393dd3d03749c6e926d9ff
yt7979ty-ui <yt79.79ty@gmail.com>
2025-11-12 20:17:31 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ★★★ この 'base' の行を追加しました ★★★
+  base: '/frontend-wedding-quiz-app/',
+  
   plugins: [react()],
   server: {
-    // サーバーを 5173 ポートで起動する設定
     port: 5173,
-    // '192.168.100.23' を指定することで、同じネットワーク上の他のデバイス
-    // (スマートフォンなど) からPCのIPアドレスでアクセスできるようになります。
     host: '0.0.0.0',
-    // サーバー起動時に自動でブラウザを開く
     open: true,
   }
 })
